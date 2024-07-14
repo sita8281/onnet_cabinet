606d5ee5440a87787799c1d63542ad2cf87e4e1e
sita8281 <mrsit8281@yandex.ru>
2024-07-14 14:09:10 +0300

    adding post request

--- deil_api/deil_async_api.py
+++ deil_api/deil_async_api.py
@@ -8,11 +8,26 @@ class BaseAPI:
     def __init__(self, config):
         self.host = config['host']
         self.port = config['port']
+        self.login = 'artem8281'
+        self.passw = '20012001'
         self.client = AsyncClient()
+        self.cookie = ''
 
     async def _get_bs(self, url):
-        session_cookie = await self._auth('artem8281', '20012001')
-        response = await self.client.get(f'http://{self.host}:{self.port}{url}', cookies={'PHPSESSID': session_cookie})
+        if not self.cookie:
+            self.cookie = await self._auth(self.login, self.passw)
+        response = await self.client.get(f'http://{self.host}:{self.port}{url}', cookies={'PHPSESSID': self.cookie})
+        response.encoding = 'cp1251'
+        return BeautifulSoup(response.text, 'lxml')
+
+    async def _post_bs(self, url: str, data: dict):
+        if not self.cookie:
+            self.cookie = await self._auth(self.login, self.passw)
+        response = await self.client.post(
+            f'http://{self.host}:{self.port}{url}',
+            cookies={'PHPSESSID': self.cookie},
+            data=data
+        )
         response.encoding = 'cp1251'
         return BeautifulSoup(response.text, 'lxml')
 
@@ -36,27 +51,69 @@ class BaseAPI:
             raise exceptions.AuthFailed()
         return cookie
 
-    async def get_user_info(self):
+    async def get_user_info(self) -> list:
+        info_list = []
         bs = await self._get_bs(url='/cabinet/userinfo')
-        print(bs)
+        table_info = bs.find('table', {'id': 'ideco_user_info'})
+        if table_info:
+            for tr in table_info.find_all('tr'):
+                key = tr.find('td', {'class': 'label'}).get_text().strip()
+                val = tr.find('td', {'class': 'value'}).get_text().strip()
+                info_list.append((key, val))
+        return info_list
 
-    async def get_payment_info(self):
+    async def get_payment_info(self) -> int | None:
         bs = await self._get_bs(url='/cabinet/payment')
-        print(bs)
+        try:
+            summ = int(bs.find('input', {'id': 'amount-sample-3'})['value'])
+        except ValueError:
+            return
+        return summ
 
     async def get_operations_info(self):
         bs = await self._get_bs(url='/cabinet/operations')
-        print(bs)
+        stat_table = bs.find('table', {'id': 'stat_table'})
+        stat_list = [tuple([td.get_text() for td in tr.find_all('td')]) for tr in stat_table.find_all('tr')]
+        return list(filter(lambda x: x, stat_list))
 
-    async def get_control_info(self):
+    async def get_sms_phone(self):
         bs = await self._get_bs(url='/cabinet/control')
-        print(bs)
+        phone = bs.find('input', {'name': 'new_sms'})['value']
+        print(phone)
 
-    async def get_tarif_info(self):
+    async def get_tarif_info(self) -> dict[str: list[tuple[str, str]], str: tuple]:
+        """
+        Получить список доступных тарифов, и текущий подключённый
+        :return:
+        """
+        tarif_list = []
+        current_tarif = ()
         bs = await self._get_bs(url='/cabinet/tarif')
-        print(bs)
+        tarifs = bs.find('select', {'name': 'new_plan_id'})
+        if not tarifs:
+            return
+        for option in tarifs.find_all('option'):
+            name = option.get_text()
+            val = option['value']
+            tarif_list.append((name.strip(), val))
+        current_tarif = bs.find('tr', {'class': 'odd'})
+        if current_tarif:
+            tds = current_tarif.find_all('td')
+            name = tds[0].get_text().strip()
+            price = tds[1].get_text().strip()
+            current_tarif = (name, price)
+        return {'tarifs': tarif_list, 'current': current_tarif}
+
+    async def set_number_phone(self, phone: str) -> bool:
+        """Установить номер телефона для SMS"""
+        bs = await self._post_bs('/cabinet/control', {'new_sms': phone, 'change_info': 'info'})
+        if str(bs).find('alert'):
+            return True
+
+
+
 
 
 loop = asyncio.get_event_loop()
 api = BaseAPI(config={'host': '192.168.255.100', 'port': '80'})
-loop.run_until_complete(api.get_control_info())
+loop.run_until_complete(api.set_number_phone(phone='9998887722'))
